2006f0fc64e09d480fbdf71d9e9939cd13833ca2
Mark <markbiz31@yahoo.com>
2015-02-02 14:15:41 -0500

    12 done

--- assignment12.rb
+++ assignment12.rb
@@ -2,23 +2,13 @@
 Create an array of questions (Strings)
 Iterate over the array using each, asking each question to the user
 Bonus: Save all the answers with the questions, and print a summary at the end
+
+What I'm looking for here is one array with multiple questions. Then as the 2nd bullet indicates, you would use the each command to iterate over them and ask them all. C
 =end
-question1_user = ["What is your name?"]
-question1_user.each do |x|
-  puts x
-question1_user = gets.chomp
-
-question2_user = ["Where do you live?"]
-question2_user.each do |y|
-  puts y
-question2_user = gets.chomp
-
-question3_user = ["What is your favorite food?"]
-question3_user.each do |z|
-  puts z
-question3_user = gets.chomp
-
-print "Your name is #{question1_user}.  You live at #{question2_user}.  Your favorite food is #{question3_user}"
-end
-end
-end
+question_user = ["What is your name?", "Where do you live?", "What is your favorite food?"]
+question_user.each
+  puts question_user
+user_input1 = gets.chomp
+user_input2 = gets.chomp
+user_input3 = gets.chomp
+print "Your name is #{user_input1}.  You live at #{user_input2}.  Your favorite food is #{user_input3}"
