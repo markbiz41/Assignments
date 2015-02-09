28136df9d0bb6fb2f3a21659c9f1535521454ba8
Mark <markbiz31@yahoo.com>
2015-02-08 21:04:22 -0500

    17 done

--- 17.rb
+++ 17.rb
@@ -1,8 +1,9 @@
-require 'json'
-file = File.read('questions.json')
-data_hash = JSON.parse(file)
-
-file = File.new("question.txt")
-question = file.readlines
-question.each
+question_user = ["What is your name?", "Where do you live?", "What is your favorite food?"]
+question_user.each do |question|
   puts question
+  answer = gets.chomp
+end
+
+require 'json'
+file = File.read('question.json')
+data_hash = JSON.parse(file)
