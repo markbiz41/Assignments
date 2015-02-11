5b1444ebac255832f60c79cf92de9c96c56e8046
Mark <markbiz31@yahoo.com>
2015-02-11 17:55:21 -0500

    18 redone

--- 18.rb
+++ 18.rb
@@ -1,8 +1,22 @@
 require 'net/http'
 require 'json'
-puts "What movie title do you want? "
-user_input = gets.chomp
-uri = URI('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=gtygr6ewkmmygvcqngsj6cpt&q=')
-response = Net::HTTP.get(uri)
-puts response
-parsed_data = JSON.parse(response)
+
+QUERY_URL = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json'
+API_KEY = 'gtygr6ewkmmygvcqngsj6cpt'
+NUM_OF_MOVIES = 1
+FORMAT = 'json'
+
+puts "What movie would you like to search for?"
+search_results = gets.chomp
+
+s = QUERY_URL + "?apiKey=#{API_KEY}&q=#{search_results}"
+puts s
+raw_json = Net::HTTP.get(URI(s))
+puts raw_json
+
+search_results = JSON.parse(raw_json)
+parsed_data = JSON.parse(raw_json)
+parsed_data.each do
+end
+
+# http://api.rottentomatoes.com/api/public/v1.0/movies.json?apiKey=gtygr6ewkmmygvcqngsj6cpt&q=grease
