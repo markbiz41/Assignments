fe0b56739aabd199ff95db4bd2f248605cf06047
Mark <markbiz31@yahoo.com>
2015-02-12 14:54:51 -0500

    14 done

--- 14.rb
+++ 14.rb
@@ -5,43 +5,26 @@
 class PigLatin
   VOWELS =  %w(a e i o u)
 
-  def self.pigatize(passed_text)
+  def self.pigatize(a)
 
     # Check to see if the first letter is a vowel, if not it's a consonant
-    if PigLatin.starts_with_vowel(passed_text[0])
-      passed_text[0] == letter["a"]
-      passed_text[0] == letter["e"]
-      passed_text[0] == letter["i"]
-      passed_text[0] == letter["o"]
-      passed_text[0] == letter["u"]
+    if PigLatin.starts_with_vowel(a[0])
       # this is placeholder code and should take action based on it starting with a vowel
-      pigatized_text = passed_text
+      pigatized_text = a + "way"
     else
       # this is placeholder code and should take action based on it starting with a consonant
-      passed_text[0] == !letter["a"]
-      passed_text[0] == !letter["e"]
-      passed_text[0] == !letter["i"]
-      passed_text[0] == !letter["o"]
-      passed_text[0] == !letter["u"]
-      pigatized_text = passed_text
+      pigatized_text = a[1..-1] + a[0] + "ay"
     end
     return pigatized_text
   end
 
   # Check to see if the first letter is a vowel
   def self.starts_with_vowel(first_letter)
-    if self.starts_with_vowel(first_letter)
+    return VOWELS.include?(first_letter)
     # this should determine if it starts with a vowel
-    return first_letter
   end
 end
 
 puts "Please enter a word and I will translate to pig latin"
-text = gets.chomp.split()
-passed_text = text
-if passed_text[0] == ["a", "e", "i", "o", "u"]
-  print passed_text + ["way"]
-else
-  print passed_text + ["ay"]
-end
-end
+text = gets.chomp
+puts "Pigatize: #{PigLatin.pigatize(text)}"
