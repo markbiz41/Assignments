e2f730ed15a9a9bf7235ed115d858f81e983f895
Mark <markbiz31@yahoo.com>
2015-02-13 14:09:22 -0500

    it works

--- 17.rb
+++ 17.rb
@@ -1,10 +1,8 @@
 require 'json'
 file = File.read('questions.json')
 data_hash = JSON.parse(file)
-file = File.new("question.txt")
-question = file.readlines
   ary = Array.new
-question.each do |q|
+data_hash['questions'].each do |q|
   puts q
   answer = gets.chomp
   ary << answer
@@ -12,3 +10,20 @@ end
   ary.each do |answer|
   puts answer
 end
+
+
+=begin
+
+puts "What movie would you like to search for?"
+search_results = gets.chomp
+
+s = QUERY_URL + "?apiKey=#{API_KEY}&q=#{search_results}"
+raw_json = Net::HTTP.get(URI(s))
+
+search_results = JSON.parse(raw_json)
+puts "\n"
+search_results['movies'].each do |movie|
+  puts movie["title"] + " - " + movie["synopsis"]
+end
+
+=end
