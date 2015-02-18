edb117332c5b6b59c8fa517885b9ca0e52ec0b47
Mark <markbiz31@yahoo.com>
2015-02-18 17:06:23 -0500

    assignment #31

--- movie_query/app/controllers/movie_query_controller.rb
+++ movie_query/app/controllers/movie_query_controller.rb
@@ -8,6 +8,16 @@ class MovieQueryController < ApplicationController
 
     s = QUERY_URL + "?apiKey=#{API_KEY}&q=#{query}"
     raw_json = Net::HTTP.get(URI(s))
-    @movies = JSON.parse(raw_json)
+    movies = JSON.parse(raw_json)
+
+    @movie = movies['movies'][0]
+    # @title = movies['title'][0]
+    # @posters = posters['Poster'][0]
+    # @runtime = runtime['Runtime'][0]
+    # @ratings = ratings['Ratings'][0]
+    # @synopsis = synopsis['Synopsis'][0]
+    # @abridged_cast = abridged_cast['Cast Members'][0]
+
+    
   end
 end
