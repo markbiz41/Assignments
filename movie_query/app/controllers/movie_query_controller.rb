c8155d5be43ee32a8f76e33c8b1d5a425e16fcdb
Mark <markbiz31@yahoo.com>
2015-02-18 17:09:29 -0500

    assignment #31 redone

--- movie_query/app/controllers/movie_query_controller.rb
+++ movie_query/app/controllers/movie_query_controller.rb
@@ -11,13 +11,5 @@ class MovieQueryController < ApplicationController
     movies = JSON.parse(raw_json)
 
     @movie = movies['movies'][0]
-    # @title = movies['title'][0]
-    # @posters = posters['Poster'][0]
-    # @runtime = runtime['Runtime'][0]
-    # @ratings = ratings['Ratings'][0]
-    # @synopsis = synopsis['Synopsis'][0]
-    # @abridged_cast = abridged_cast['Cast Members'][0]
-
-    
   end
 end
