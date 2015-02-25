07af180e1e9411e94f11e99dde8afb4f623f7d0e
Mark <markbiz31@yahoo.com>
2015-02-25 17:04:39 -0500

    movie query app

--- movie_query/app/controllers/movie_query_controller.rb
+++ movie_query/app/controllers/movie_query_controller.rb
@@ -9,7 +9,14 @@ class MovieQueryController < ApplicationController
     s = QUERY_URL + "?apiKey=#{API_KEY}&q=#{query}"
     raw_json = Net::HTTP.get(URI(s))
     movies = JSON.parse(raw_json)
-
     @movie = movies['movies'][0]
   end
 end
+
+
+=begin
+    query = JSON.PARSE(raw_json)
+    post "\n"
+    query['movies'].each do |movie|
+    @movie = movies['movies'][0]
+=end
