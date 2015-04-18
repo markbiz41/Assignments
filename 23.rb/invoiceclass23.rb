a0a2a81b6f20fa1d4062ae01a35257524a0e08aa
Mark <markbiz31@yahoo.com>
2015-04-18 15:05:37 -0400

    23.rb fixed

--- 23.rb/invoiceclass23.rb
+++ 23.rb/invoiceclass23.rb
@@ -13,12 +13,53 @@ class Invoice
     @items = new_items
   end
 
+
   def sub_total
-    @s = sales_price * quantity
+    total = 0.00
+
+    @items.each do |item|
+      item_cost = item.sales_price * item.quantity
+      total = total + item_cost
+    end
+
+    return total
+    #user_total.select {|quantity| quantity =~ 0}
+  end
+
+  def tax_total
+    tax = 0.00
+
+    @items.each do |item|
+      item_tax = item.sales_price * item.quantity * item.tax_percentage
+      tax = tax + item_tax
+    end
+
+    return tax
+  end
+
+  def total
+    self.sub_total + self.tax_total
+  end
+
+  def check_items
+    @items.reject! {|item| item.quantity == 0}
+  end
+
+
+  def print
+    self.check_items
+    @items.each do |invoice_item|
+      puts "The product name is: #{invoice_item.product_name}"
+      puts "The sales price is: #{invoice_item.sales_price}"
+      puts "The quantity amount is: #{invoice_item.quantity}"
+      puts "The tax percentage is: #{invoice_item.tax_percentage}"
+    end
+
+    puts "Your Purchase Sub-Total is: %.2f" % self.sub_total # Prints 20.47 (and no more)
+
+    puts "Tax: %.2f" % self.tax_total
+
+    puts "Your Grand Total is: %.2f" % self.total
   end
 
 end
-
-=begin
-10. Show a sub-total without the tax, then a line item for the tax, then the total.
-=end
--- 23.rb/main.rb
+++ 23.rb/main.rb
@@ -22,12 +22,11 @@ loop do
 
   # assign it to the invoice
 
-  invoice.items << invoice_item
   invoice_item.product_name = user_product
   invoice_item.sales_price = user_price
   invoice_item.quantity = user_quantity
   invoice_item.tax_percentage = user_tax
-
+  invoice.items << invoice_item
 
 
   # assign user input to invoice_item attributes:
@@ -36,26 +35,4 @@ loop do
   break if user_shopping == "no"
 end
 
-invoice_total = 0
-tax_total = 0
-
-invoice.items.each do |invoice_item|
-  puts "The product name is: #{invoice_item.product_name}"
-  puts "The sales price is: #{invoice_item.sales_price}"
-  puts "The quantity amount is: #{invoice_item.quantity}"
-  puts "The tax percentage is: #{invoice_item.tax_percentage}"
-
-  item_cost = invoice_item.sales_price * invoice_item.quantity
-
-  invoice_total += item_cost
-  tax_total += item_cost * invoice_item.tax_percentage
-end
-
-
-
-puts "Your Purchase Sub-Total is: %.2f" % invoice_total # Prints 20.47 (and no more)
-
-puts "Tax: %.2f" % tax_total
-
-grand_total = invoice_total + tax_total
-puts "Your Grand Total is: %.2f" % grand_total
+invoice.print
